da708160dcf8f16140b3a1268e6ab9ffcd523eef
kenshinsee <kenshinsee@hotmail.com>
2016-03-13 00:08:21 +0800

    0313

--- Stock/bin/download_stock_eod.py
+++ Stock/bin/download_stock_eod.py
@@ -52,8 +52,9 @@ def get_stock_list(conn):
 		stocks.append(row['id'])
 	return stocks
 
-def download_to_file(stocks, stock_obj_name, fh, log_fh, warn_fh):
+def download_to_file(stocks, stock_obj_name, to_file, log_fh, warn_fh):
 	#-- iterate stocks, download eod data from webside
+	fh = open(to_file, 'a')
 	for s in stocks:
 		#-- call method of stock object to get content of url
 		print_log('%(object)s("%(stock)s", "dummy", "dummy")' % {'object': stock_obj_name, 'stock': s})
@@ -67,7 +68,59 @@ def download_to_file(stocks, stock_obj_name, fh, log_fh, warn_fh):
 		except KeyError:
 			warn_log(s[0:2] + ' is not setup in ' + stock_obj_name, warn_fh)
 			continue
+	fh.close()
+	
+def insert_into_table(db_field_yaml, in_file, stock_object, conn):
+	# based on the fields mapping between db and object, db type defined in yaml, generate delete sql and insert sql, and fire to db
+	# this function could be used for any db insert, if yaml and object are setup properly
+	# python download_stock_eod.py -m load -f D:\\workspace\\Stock\\data\\stock_daily\\tengxun_20160304.txt
+	db_field_mapping = get_yaml(db_field_yaml)
+	tab_name = os.path.basename(db_field_yaml).replace('.yml', '') # yml file name as table name
+	tab_fields = [] # table field names
+	tab_pk = [] # table pk
+	tab_types = [] # table field types
+	obj_attrs = [] # attribute names in stock object
+	for k,v in db_field_mapping.items():
+		tab_type = v['type']
+		obj_attr = v['stock_object'][stock_object]
+		if obj_attr != None: # If None|Null is set for fields in yml, remove the fields from insertion
+			tab_fields.append(k)
+			if v['is_pk'] == 'Y': tab_pk.append(k) # pk, delete before insert
+			tab_types.append(tab_type)
+			obj_attrs.append(obj_attr)
+	del_sql = 'delete from {tab_name} where 1=1 '.format(tab_name=tab_name)
+	ins_sql = 'insert into {tab_name}({fields}) '.format(tab_name=tab_name, fields=','.join(tab_fields))
+	# iterate each row in the file, insert into table
+	with open(in_file) as f:
+		for row in f.readlines():
+			# get_stock_object_from_str is a function should be available in all the stock objects
+			# this function accepts the string returned from website and generate a dict for stock object
+			# the dict is like {stock: {date: object}}
+			stock_dict = eval('{object}.get_stock_object_from_str(row)'.format(object=stock_object, row=row))
+			for stock in stock_dict: # for Tengxun or sina interface, there is just one stock in one stock dict
+				for date in stock_dict[stock]: # for Tengxun or sina interface, there is just one date in one stock dict
+					stock_obj = stock_dict[stock][date] # this object is stock implementation object
+					value_sql = reduce(lambda x, y: ( x if re.match(r'stock_obj', x) else 'stock_obj.' + x + ', ' ) + "stock_obj.{attr_name}, ".format(attr_name=y), obj_attrs) # add 'stock_obj.' to the first attr, and concatenate attrs to a string
+					value_sql = value_sql[0:-2] # remove the last comma and the blankspace next to it
+					value_sql = eval(value_sql) # tupe returned
+					final_value_sql = ''
+					del_where = ''
+					for i, v in enumerate(value_sql):
+						value = "'" + v + "'" if tab_types[i] == 'date' or tab_types[i] == 'varchar' else 'Null' if len(str(v)) == 0 else str(v) # date and varchar quoted by single quote, otherwise no quote or null(if length of value is 0)
+						final_value_sql = final_value_sql + value + ', '
+						if tab_fields[i] in tab_pk: 
+							del_where = del_where + ' and {field}={value}'.format(field=tab_fields[i], value=value)
+					final_value_sql = final_value_sql[0:-2]
+					del_complete_sql = del_sql + del_where
+					ins_complete_sql = ins_sql + ' values( ' + final_value_sql + ')'
+					print_log('Deleting [{stock},{date}] from {tab_name}...\n {sql}'.format(stock=stock,date=date,tab_name=tab_name,sql=del_complete_sql), log_fh)
+					print_log('Inserting [{stock},{date}] into {tab_name}...\n {sql}'.format(stock=stock,date=date,tab_name=tab_name,sql=ins_complete_sql), log_fh)
+					#cur = get_cur(conn)
+					#cur.execute(ins_complete_sql)
+					#conn.commit()
+					sys.exit()
 
+					
 #-- parse input parameter, var assignment
 stock_object = {
 	"tengxun": 'Tengxun_stock',
@@ -113,54 +166,15 @@ conn = get_conn(db_dict["DB"], db_dict["Username"], db_dict["Password"], db_dict
 
 #-- download stock info from internet
 if options.mode == 'download' or options.mode == 'downloadAndLoad':
-	#-- stock list
+	#-- stock list fetch from dw.dim_stock
 	stocks = get_stock_list(conn)
-	#-- open file
-	fh = open(file_full_name, 'a')
-	download_to_file(stocks, stock_object[options.object_class], fh, log_fh, warn_fh)
-	fh.close()
+	download_to_file(stocks, stock_object[options.object_class], file_full_name, log_fh, warn_fh)
 
 #-- load stock info into database
 if options.mode == 'load' or options.mode == 'downloadAndLoad':
-	# python download_stock_eod.py -m load -f D:\\workspace\\Stock\\data\\stock_daily\\tengxun_20160304.txt
-	db_field_mapping = get_yaml(STOCK_YML)
-	tab_name = os.path.basename(STOCK_YML).replace('.yml', '') # yml file name as table name
-	tab_fields = [] # table field names
-	tab_types = [] # table field types
-	obj_attrs = [] # attribute names in stock object
-	for k,v in db_field_mapping.items():
-		tab_type = v['type']
-		obj_attr = v['stock_object'][stock_object[options.object_class]]
-		if obj_attr != None: # If None|Null is set for fields in yml, remove the fields from insertion
-			tab_fields.append(k)
-			tab_types.append(tab_type)
-			obj_attrs.append(obj_attr)
-	
-	print_log('Start to load data into {tab_name}...'.format(tab_name=tab_name), log_fh)
-	ins_sql = 'insert into {tab_name}({fields}) '.format(tab_name=tab_name, fields=','.join(tab_fields))
+	insert_into_table(STOCK_YML, file_full_name, stock_object[options.object_class], conn)
 
-	with open(file_full_name) as f:
-		for row in f.readlines():
-			# get_stock_object_from_str is a function should be available in all the stock objects
-			# this function accepts the string returned from website and generate a dict for stock object
-			# the dict is like {stock: {date: object}}
-			stock_dict = eval('{object}.get_stock_object_from_str(row)'.format(object=stock_object[options.object_class], row=row))
-			for stock in stock_dict: # for Tengxun or sina interface, there is just one stock in one stock dict
-				for date in stock_dict[stock]: # for Tengxun or sina interface, there is just one date in one stock dict
-					stock_obj = stock_dict[stock][date] # this object is stock implementation object
-					value_sql = reduce(lambda x, y: ( x if re.match(r'stock_obj', x) else 'stock_obj.' + x + ', ' ) + "stock_obj.{attr_name}, ".format(attr_name=y), obj_attrs)
-					value_sql = value_sql[0:-2] # remove the last comma and the blankspace next to it
-					value_sql = eval(value_sql) # tupe returned
-					final_value_sql = ''
-					for i, v in enumerate(value_sql):
-						final_value_sql = final_value_sql + ( "'" + v + "', " if tab_types[i] == 'date' or tab_types[i] == 'varchar' else 'Null, ' if len(str(v)) == 0 else str(v) + ', ' ) # date and varchar quoted by single quote, otherwise no quote or null(if length of value is 0)
-					final_value_sql = final_value_sql[0:-2]
-					ins_complete_sql = ins_sql + ' values( ' + final_value_sql + ')'
-					print ins_complete_sql
-					cur = get_cur(conn)
-					cur.execute(ins_complete_sql)
-					conn.commit()
-					sys.exit()
+
 conn.close()
 
 #-- complete
