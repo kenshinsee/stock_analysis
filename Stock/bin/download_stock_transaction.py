980ea780d17462b47d5d608b8479f0c409d6ad7a
kenshinsee <kenshinsee@hotmail.com>
2016-04-27 01:13:28 +0800

    merge before load

--- Stock/bin/download_stock_transaction.py
+++ Stock/bin/download_stock_transaction.py
@@ -3,7 +3,7 @@
 # This script is used to download transaction data to flat file and load data from flat file to db
 
 
-import sys,os,re,datetime,time,platform
+import sys,os,re,datetime,time,platform,fnmatch,shutil
 
 from optparse import OptionParser
 from urllib2 import HTTPError
@@ -13,6 +13,7 @@ from Sys_paths import Sys_paths
 from tooling.psql import get_conn, get_cur
 from downloader.Stock_trans_downloader import Stock_trans_downloader
 from loader.Stock_trans_loader import Stock_trans_loader
+from tooling.db_func import inserter, get_query_result, psql_copy_from
 
 
 #-- sys var
@@ -24,17 +25,23 @@ data_dir = Sys_paths.DATA_STOCK_TRANSACTION
 YML_DIR = Sys_paths.YML_DIR
 LOG_DIR = Sys_paths.LOG_DIR
 DB_YML = YML_DIR + SEP + "db.yml"
-STOCK_YML = YML_DIR + SEP + "table" + SEP + "dw.stock_transaction.yml"
 now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 QUEUE_DOWNLOAD_MAX_SIZE = 3
 QUEUE_LOAD_MAX_SIZE = 2
 
 #-- fetch DB info
 db_dict = get_yaml(DB_YML)
+DB_NAME = db_dict["DB"]
+DB_HOST = db_dict["Host"]
+DB_PORT = db_dict["Port"]
+DB_UNAME = db_dict["Username"]
+DB_PWD = db_dict["Password"]
+
 #-- open db connection
 conn = get_conn(db_dict["DB"], db_dict["Username"], db_dict["Password"], db_dict["Host"], db_dict["Port"])
 
 recent_working_day = recent_working_day(is_skip_holiday=True, conn=conn)
+
 
 #-- opts
 parser = OptionParser()
@@ -43,6 +50,7 @@ parser.add_option("--start_date", "-s", dest="start_date", action="store", defau
 parser.add_option("--end_date", "-e", dest="end_date", action="store", default=recent_working_day, help="The default value is " + recent_working_day + ", the format is YYYYMMDD")
 parser.add_option("--stock_id", "-i", dest="stock_id", action="store", help="--stock_id|-i is optional")
 parser.add_option("--obj_selection", "-o", dest="obj_selection", action="store", help="--obj_selection|-o can indicate which object interface you want to use, T:Tengxun, S:Sina, N:Netease, e.g. -o \"T|S\"")
+parser.add_option("--merge_before_copy", "-g", dest="merge_before_copy", action="store_true", default=True, help="Merge all the files in the data directory into one file and load it into table")
 #parser.add_option("--enable_copy", "-c", dest="enable_copy", action="store_true", default=True if platform.system() == 'Linux' else False, help="Enable postgres copy when loading data into table")
 parser.add_option("--enable_copy", "-c", dest="enable_copy", action="store_true", default=True, help="Enable postgres copy when loading data into table")
 (options, args) = parser.parse_args()
@@ -168,7 +176,8 @@ def load_log_checker(conn, start_date=options.start_date, end_date=options.end_d
     return len(rows)
 
     
-def loader(queue, conn, start_date=options.start_date, end_date=options.end_date, stock_id=options.stock_id, enable_copy=options.enable_copy):
+def loader(queue, conn, start_date=options.start_date, end_date=options.end_date, stock_id=options.stock_id, merge_before_copy=options.merge_before_copy, enable_copy=options.enable_copy):
+
     cur_date_dt = datetime.datetime.strptime(start_date,'%Y%m%d')
     end_date_dt = datetime.datetime.strptime(end_date,'%Y%m%d')
     
@@ -183,27 +192,71 @@ def loader(queue, conn, start_date=options.start_date, end_date=options.end_date
     
     cur = get_cur(conn)
     while cur_date_dt <= end_date_dt:  
-        stock_list_sql_var_replaced = stock_list_sql.format(biz_date=cur_date_dt)
-        cur.execute(stock_list_sql_var_replaced)
-        rows = list(cur)
-        for row in rows:
-            row_id = row['row_id']
-            biz_date = str(row['biz_date']).replace('-','')
-            stock_id = row['stock_id']
-            while queue.full():
-                print_log('=================> queue is full, wait for 1 second...')
-                time.sleep(1)
-            s = Stock_trans_loader(queue, conn, row_id, stock_id, biz_date, enable_copy=enable_copy )
-            s.start()
-            print_log('-----> queue size: ' + str(queue.qsize()))
+        if merge_before_copy:
+        # since load files one by one into table is taking too much time, the solution to boost the procedure is to merge all the pieces of files into one file and load the merge file into table, this takes less than 5 mins to complete.
+            cur_date_str = cur_date_dt.strftime('%Y%m%d')
+            working_dir = data_dir + SEP + cur_date_str
+            file_merged = os.path.join(working_dir, "file_merged.csv")
+            if os.path.exists(file_merged):
+                warn_log('Removing old file: ' + file_merged)
+                os.remove(file_merged)
+            #-- Starting to merge files
+            with open(file_merged, "a") as dest:
+                i=0
+                for _, _, filenames in os.walk(working_dir):
+                    for filename in fnmatch.filter(filenames, "[0-9]*.txt"):
+                        with open(os.path.join(working_dir, filename)) as src:
+                            shutil.copyfileobj(src, dest)
+                        i+=1
+                        print_log('Merged ' + str(i) + ' files.')
+            #-- Deleting records from db
+            del_sql = '''delete from dw.stock_transaction where biz_date = '{}' '''.format(cur_date_str)
+            get_query_result(conn, del_sql)
             conn.commit()
-                
-        cur_date_dt = cur_date_dt + datetime.timedelta(1)
+            print_log('Deletion for biz_date {} completed successfully.'.format(cur_date_str))
+            #-- Updating is_load_success to N in log table
+            upd_sql = '''update dw.log_stock_transaction set is_load_success = 'N' where biz_date = '{}' and is_download_success = 'Y' '''.format(cur_date_str)
+            get_query_result(conn, upd_sql)
+            conn.commit()
+            print_log('is_load_success is updated to N')
+
+            #++++++++ Starting to load the merged file into table
+            psql_copy_from(DB_HOST, DB_NAME, DB_UNAME, 'dw.stock_transaction', file_merged, DB_PORT, args=' with (encoding \'GBK\')')
+            print_log('Successfully loaded {} into table.'.format(file_merged))
+            
+            #-- Updating is_load_success to Y in log table
+            upd_sql = '''update dw.log_stock_transaction set is_load_success = 'Y' where biz_date = '{}' and is_download_success = 'Y' '''.format(cur_date_str)
+            get_query_result(conn, upd_sql)
+            conn.commit()
+            print_log('is_load_success is updated to Y')
+
+            #-- Cleaning up working dir
+            os.remove(file_merged)
+            
+            cur_date_dt = cur_date_dt + datetime.timedelta(1)
+            
+        else:
+            stock_list_sql_var_replaced = stock_list_sql.format(biz_date=cur_date_dt)
+            cur.execute(stock_list_sql_var_replaced)
+            rows = list(cur)
+            for row in rows:
+                row_id = row['row_id']
+                biz_date = str(row['biz_date']).replace('-','')
+                stock_id = row['stock_id']
+                while queue.full():
+                    print_log('=================> queue is full, wait for 1 second...')
+                    time.sleep(1)
+                s = Stock_trans_loader(queue, conn, row_id, stock_id, biz_date, enable_copy=enable_copy )
+                s.start()
+                print_log('-----> queue size: ' + str(queue.qsize()))
+                conn.commit()
+                    
+            cur_date_dt = cur_date_dt + datetime.timedelta(1)
 
     while not queue.empty():
         print_log('=================> queue is not empty yet, wait for 1 second...')
         time.sleep(1)
-        
+            
         
 # check validation of mode
 if not (options.mode in ['download', 'load', 'downloadAndLoad']):
